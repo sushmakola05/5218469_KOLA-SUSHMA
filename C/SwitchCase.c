56e0dee36fde65f6831db58a6dc92bed0c814da7
KOLA SUSHMA <126850604+sushmakola05@users.noreply.github.com>
2025-08-04 20:30:57 +0530

    Added SwitchCase.c to C

--- C/SwitchCase.c
+++ C/SwitchCase.c
@@ -1,4 +1,4 @@
-// Printing days of the week with valid input 
+// Printing days of the week using loop and whenever there is wrong choice, prompt the user to enter again a day from 0 to 6
 
 #include <stdio.h>
 
@@ -9,17 +9,25 @@ int main() {
         printf("Enter any day b/w 0 to 6: ");
         scanf("%d", &day);
         switch (day) {
-            case 0: printf("Sunday\n"); return 0;
-            case 1: printf("Monday\n"); return 0;
-            case 2: printf("Tuesday\n"); return 0;
-            case 3: printf("Wednesday\n"); return 0;
-            case 4: printf("Thursday\n"); return 0;
-            case 5: printf("Friday\n"); return 0;
-            case 6: printf("Saturday\n"); return 0;
-            case 7: printf("Sunday\n"); return 0;
+            case 0: printf("Sunday\n"); 
+                break;
+            case 1: printf("Monday\n"); 
+                break;
+            case 2: printf("Tuesday\n"); 
+                break;
+            case 3: printf("Wednesday\n"); 
+                break;
+            case 4: printf("Thursday\n"); 
+                break;
+            case 5: printf("Friday\n"); 
+                break;
+            case 6: printf("Saturday\n"); 
+                break;
             default:
                 printf("Invalid choice. Please enter a number between 0 and 6.\n\n");
+                continue;
         }
+        break;
     }
 
     return 0;
